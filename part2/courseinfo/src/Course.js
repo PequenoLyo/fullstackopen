6a8208f4e40aa97367638a588f8983a4772eb149
Élio Maia <eliomaia0@gmail.com>
2022-11-23 22:19:36 +0000

    Adds exercises 2.2 and 2.3.

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -3,14 +3,18 @@ import Content from './Content.js';
 import Total from './Total.js';
 
 const Course = (props) => {
-
-    
-
+  const numberOfExercises = props.course.parts.reduce(
+    (s, p) => s + p.exercises,
+    0
+  );
 
   return (
     <div>
       <Header course={props.course.name} />
-      <Content parts={props.course.parts} />      
+      <Content parts={props.course.parts} />
+      <p>
+        <b>total of {numberOfExercises} exercises</b>
+      </p>
     </div>
   );
 };
