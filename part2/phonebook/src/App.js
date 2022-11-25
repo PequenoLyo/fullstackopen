7afa1207bed725f6d8d92547876586d4dd6bf6b0
Élio Maia <eliomaia0@gmail.com>
2022-11-25 22:54:34 +0000

    Adds exercises 2.8 and 2.9.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState();
 
   const handleNewEntry = (e) => {
     e.preventDefault();
     if (isDuplicatePerson(newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([
+        ...persons,
+        { name: newName, number: newNumber, id: persons.count + 1 },
+      ]);
       setNewName('');
+      setNewNumber('');
     }
   };
 
@@ -18,8 +28,12 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
   const isDuplicatePerson = (n) => {
-        return persons.some((person) => person.name === n);
+    return persons.some((person) => person.name === n);
   };
 
   return (
@@ -27,7 +41,12 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleNewEntry}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          <div>
+            name: <input value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            number: <input value={newNumber} onChange={handleNumberChange} />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -35,7 +54,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((p) => {
-        return <p key={p.name}>{p.name}</p>;
+        return <p key={p.id}>{p.name} {p.number}</p>;
       })}
     </div>
   );
