a6841ab636f6216d897aaee44f2387e9c52f1be4
Élio Maia <eliomaia0@gmail.com>
2022-12-06 22:43:08 +0000

    Adds exercise 3.8.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,11 @@ var morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'))
+
+morgan.token('body', request => {
+  return request.method === "POST" ? JSON.stringify(request.body) : " "
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
