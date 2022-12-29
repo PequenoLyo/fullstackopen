b3d99765872bed2b55f7c7561f172a013a188384
Élio Maia <eliomaia0@gmail.com>
2022-12-29 13:43:19 +0000

    Adds exercise 4.5.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,16 +1,35 @@
 const dummy = (blogs) => {
-  return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
-    return sum + item.likes
-  }
+    return sum + item.likes;
+  };
 
-  return blogs.reduce(reducer, 0)
-}
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  let currentFavoriteBlog = {title: '', author: '', likes: -1};
+  blogs.forEach((blog) => {
+    // console.log(blog)
+    if (blog.likes > currentFavoriteBlog.likes) {
+      currentFavoriteBlog = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }     
+    }
+  });
+  return currentFavoriteBlog
+};
 
 module.exports = {
   dummy,
-  totalLikes
-}
+  totalLikes,
+  favoriteBlog,
+};
