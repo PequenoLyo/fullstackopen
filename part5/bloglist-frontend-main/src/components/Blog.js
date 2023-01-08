af5a9234b6aa2841960d000bb49797c2ea9e4a2f
Élio Maia <eliomaia0@gmail.com>
2023-01-08 11:48:21 +0000

    Adds exercises 5.6 and 5.7.

--- part5/bloglist-frontend-main/src/components/Blog.js
+++ part5/bloglist-frontend-main/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  console.log(blog);
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className="blog">
+      <div>
+        <span className="blogTitle">{blog.title} - </span>
+        <span classname="blogAuthor">{blog.author}</span>{' '}
+        <button id="buttonView" onClick={toggleVisibility}>
+          {visible ? 'hide' : 'show'}
+        </button>
+      </div>
+      {visible && (
+        <div className="blogDetails">
+          <div>{blog.url}</div>
+          <div>
+            Likes: {blog.likes} <button>like</button>
+          </div>
+          {blog.user && <div>{blog.user.name}</div>}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
