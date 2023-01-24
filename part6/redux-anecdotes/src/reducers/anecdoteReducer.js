96c4e102ab444ba6791bfba91301ed6293c83d3d
Élio Maia <eliomaia0@gmail.com>
2023-01-24 22:52:05 +0000

    Adds exercise 6.13.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,66 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
+const getId = () => (100000 * Math.random()).toFixed(0)
 
 const anecdoteSlice = createSlice({
-  name: "anecdotes",
-  initialState,
+  name: 'anecdotes',
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      state.push(action.payload);
+      state.push(action.payload)
     },
     voteAnecdote(state, action) {
-      const id = action.payload;
-      const anecdoteToChange = state.find((anecdote) => anecdote.id === id);
+      const id = action.payload
+      const anecdoteToChange = state.find((anecdote) => anecdote.id === id)
       const changedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1,
-      };
+      }
       return state.map((anecdote) =>
         anecdote.id === id ? changedAnecdote : anecdote
-      );
-    },    
+      )
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
+    },
   },
-});
+})
 
 export const voteOn = (id) => {
-  console.log("voting on", id);
+  console.log('voting on', id)
   return {
-    type: "anecdotes/voteAnecdote",
+    type: 'anecdotes/voteAnecdote',
     payload: id,
-  };
-};
+  }
+}
 
 export const newAnecdote = (content) => {
   return {
-    type: "anecdotes/createAnecdote",
+    type: 'anecdotes/createAnecdote',
     payload: {
       content,
       id: getId(),
       votes: 0,
     },
-  };
-};
+  }
+}
 
-export const { createAnecdote, voteAnecdote, sortAnecdotes } =
-  anecdoteSlice.actions;
-export default anecdoteSlice.reducer;
+export const { createAnecdote, voteAnecdote, appendAnecdote, setAnecdotes } =
+  anecdoteSlice.actions
+export default anecdoteSlice.reducer
